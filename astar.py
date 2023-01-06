ec10bcbbc82deffec093b0e484df61e98f002b6a
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-05 23:40:31 -0800

    added right click functionality

--- astar.py
+++ astar.py
@@ -148,16 +148,25 @@ def main (win, width):
                 pos = pygame.mouse.get_pos()
                 row,col = get_clicked_pos(pos, ROWS, width)
                 node = grid[row][col]
-                if not start:
+                if not start and node != end:
                     start = node
                     start.make_start()
-                elif not end:
+                elif not end and node != start:
                     end = node
                     end.make_end()
                 elif node != end and node != start:
                     node.make_barrier()
+
             elif pygame.mouse.get_pressed()[2]:  #checks if the right mouse button is pressed
-                pass
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+                    
     pygame.quit()
 
 main(WIN, WIDTH)
