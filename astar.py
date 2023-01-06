b706e04511e22100b37c20e13968488096d5e5a8
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-05 23:31:41 -0800

    fixed missing width attribute for Node class

--- astar.py
+++ astar.py
@@ -6,7 +6,6 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption ("A* Path Finding Algorithm")
 
-
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -26,6 +25,7 @@ class Node:
         self.y = col * width
         self.color = WHITE
         self.neighbors = []
+        self.width = width
         self.total_rows = total_rows
 
     def get_pos(self):
